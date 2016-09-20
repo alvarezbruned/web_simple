838fc3b18048c8d248e51bfb47e11027c55825b0
alvarezbruned <albert@alvarezbruned.com>
2016-09-20 13:36:00 -0500

    Update http.go

--- http.go
+++ http.go
@@ -19,7 +19,8 @@ func main() {
         fmt.Fprintf(os.Stdout, "I'm host %s with port %s \n", hostname, port)
  	fmt.Fprintf(w, "I'm host %s with port %s \n", hostname, port)
     })
-
+    fs := http.FileServer(http.Dir("/app/www"))
+    http.Handle("/", fs)
     http.HandleFunc("/", serveTemplate)
     log.Fatal(http.ListenAndServe(":" + port, nil))
     
@@ -29,6 +30,32 @@ func serveTemplate(w http.ResponseWriter, r *http.Request) {
   lp := path.Join("www", "index.htm")
   fp := path.Join("www", r.URL.Path)
 
-  tmpl, _ := www.ParseFiles(lp, fp)
-  tmpl.ExecuteTemplate(w, "layout", nil)
+  // Return a 404 if the template doesn't exist
+  info, err := os.Stat(fp)
+  if err != nil {
+    if os.IsNotExist(err) {
+      http.NotFound(w, r)
+      return
+    }
+  }
+
+  // Return a 404 if the request is for a directory
+  if info.IsDir() {
+    http.NotFound(w, r)
+    return
+  }
+
+  tmpl, err := template.ParseFiles(lp, fp)
+  if err != nil {
+    // Log the detailed error
+    log.Println(err.Error())
+    // Return a generic "Internal Server Error" message
+    http.Error(w, http.StatusText(500), 500)
+    return
+  }
+
+  if err := tmpl.ExecuteTemplate(w, "layout", nil); err != nil {
+    log.Println(err.Error())
+    http.Error(w, http.StatusText(500), 500)
+  }
 }
