ed5f5ac71710e948153b229941a011181c285b76
alvarezbruned <albert@alvarezbruned.com>
2016-09-20 13:55:08 -0500

    Update http.go

--- http.go
+++ http.go
@@ -25,37 +25,3 @@ func main() {
     log.Fatal(http.ListenAndServe(":" + port, nil))
     
 }
-
-func serveTemplate(w http.ResponseWriter, r *http.Request) {
-  lp := path.Join("/app/www", "index.htm")
-  fp := path.Join("/app/www", r.URL.Path)
-
-  // Return a 404 if the template doesn't exist
-  info, err := os.Stat(fp)
-  if err != nil {
-    if os.IsNotExist(err) {
-      http.NotFound(w, r)
-      return
-    }
-  }
-
-  // Return a 404 if the request is for a directory
-  if info.IsDir() {
-    http.NotFound(w, r)
-    return
-  }
-
-  tmpl, err := template.ParseFiles(lp, fp)
-  if err != nil {
-    // Log the detailed error
-    log.Println(err.Error())
-    // Return a generic "Internal Server Error" message
-    http.Error(w, http.StatusText(500), 500)
-    return
-  }
-
-  if err := tmpl.ExecuteTemplate(w, "layout", nil); err != nil {
-    log.Println(err.Error())
-    http.Error(w, http.StatusText(500), 500)
-  }
-}
